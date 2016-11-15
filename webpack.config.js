55401e871e4deafe4b7736f240975ce176bbbb20
Grgur Grisogono <grgur@grgur.com>
2016-11-15 15:00:35 +0100

    no need for build.js, no chunks in dev server output

--- webpack.config.js
+++ webpack.config.js
@@ -100,8 +100,21 @@ module.exports = {
     historyApiFallback: true,
     port: 3000,
     compress: isProd,
-    stats: { colors: true },
     inline: !isProd,
     hot: !isProd,
+    stats: {
+      assets: true,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: false,
+      timings: true,
+      version: false,
+      warnings: true,
+      colors: {
+        green: '\u001b[32m',
+      }
+    },
   }
 };
