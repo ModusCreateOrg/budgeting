d5f71c236bb18acd1d08103fb5a97f4cb5d49ef3
Brice Mason <bricemason@gmail.com>
2017-03-03 10:33:04 -0500

    [#140925095] bootstrap blank page (#21)
    - all /components deleted
    - transaction containers deleted
    - removed redux wire-up in App
    - renamed title in index page
    - removed css
    - left existing base redux code (actions, reducers, store, default data definitions)

--- client/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ()=> {
-  return (
-    <header>
-      <h1>Budget</h1>
-    </header>
-  );
-};
--- client/containers/App/index.js
+++ client/containers/App/index.js
@@ -1,63 +1,8 @@
 import React, { Component, PropTypes } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-
-import Header from 'components/Header';
-import Grid from 'components/Grid';
-import TransactionForm from 'containers/TransactionForm';
-import TransactionSummary from 'containers/TransactionSummary';
-import * as AppActions from 'actions';
 import './style.css';
 
-class App extends Component {
-  static propTypes = {
-    transactions: PropTypes.array,
-    summary: PropTypes.object,
-    gridFields: PropTypes.array,
-    actions: PropTypes.object
-  };
-
-  componentWillMount() {
-    const { transactions, actions } = this.props;
-    actions.requestSum(transactions);
-  }
-
+export default class App extends Component {
   render() {
-    const {
-      transactions,
-      gridFields,
-      summary,
-      actions
-    } = this.props;
-
-    return (
-      <div className="viewport">
-        <Header addTodo={actions.addTodo} />
-        <Grid fields={gridFields} data={transactions}>
-          <TransactionForm action={actions.addTransaction}/>
-          <TransactionSummary data={summary} fields={gridFields} />
-        </Grid>
-      </div>
-    );
+    return <div></div>
   }
 }
-
-function mapStateToProps(state) {
-  const { transactions } = state;
-  return {
-    transactions: transactions.transactions,
-    summary: transactions.summary,
-    gridFields: transactions.transactionsGrid
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(AppActions, dispatch)
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
