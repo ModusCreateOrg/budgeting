3b62e98edb64cd46a161d065624b4d82596e48b3
Grgur Grisogono <grgur@grgur.com>
2017-03-06 22:22:27 +0100

    Linting as part of CI test (#24)
    * add to circleci
    
    * lint free

--- client/containers/App/index.js
+++ client/containers/App/index.js
@@ -1,8 +1,8 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
 export default class App extends Component {
   render() {
-    return <div></div>
+    return <div />;
   }
 }
