f10a778a94d6ea4a8f72d3fd869e7f66b1001830
Jason Malfatto <jmalfatto@gmail.com>
2017-03-07 11:59:40 -0500

    adds mock data

--- client/containers/App/index.js
+++ client/containers/App/index.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux'; 
+
 import Header from 'components/Header';
+import * as AppActions from 'actions';
 import './style.scss';
 
 const Budget = () => <h1>Budget</h1>;
 const Reports = () => <h1>Reports</h1>;
 
-export default class App extends Component {
+class App extends Component {
+  componentWillMount() {
+    const { transactions, categories } = this.props;
+
+    transactions.forEach(trans => console.log(trans, categories[trans.categoryId]));
+  }
 
   render() {
     return (
@@ -22,3 +31,20 @@ export default class App extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  const { transactions } = state;
+
+  return {
+    transactions: transactions.transactions,
+    categories: transactions.categories
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(AppActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
