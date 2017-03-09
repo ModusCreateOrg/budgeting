925e0d9e63acb19f6190c80650d6d1333c9f5ae8
Timothy Eagan <tim@moduscreate.com>
2017-03-09 15:24:59 -0500

    refactor app to use ducks

--- client/store/index.js
+++ client/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-import rootReducer from 'reducers';
+import rootReducer from 'modules/reducer';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(rootReducer);
