03508850ddb5a878e11b128e361f71399a67111d
Timothy Eagan <tim@moduscreate.com>
2017-03-10 10:12:12 -0500

    Circle CI fix
    - change name of reducer
    - add redux_devtools_extension

--- client/store/index.js
+++ client/store/index.js
@@ -1,9 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import rootReducer from 'modules/reducer';
+import rootReducer from 'modules/rootReducer';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const store = createStoreWithMiddleware(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 export default store;
