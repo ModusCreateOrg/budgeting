86bc07b6ab2cc911a159417ee90ceb51af39ce56
Grgur Grisogono <grgur@grgur.com>
2017-03-15 14:32:36 +0100

    fix lint stuff

--- client/modules/transactions.js
+++ client/modules/transactions.js
@@ -41,7 +41,7 @@ export const actions = {
  */
 
 function getNextTransactionID(state) {
-  return state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1
+  return state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1;
 }
 
 // Add a new transaction.
