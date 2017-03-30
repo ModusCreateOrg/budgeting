96410bea77b5705ef48d6ea0da9469eea7dbb36a
Jason Malfatto <jmalfatto@gmail.com>
2017-03-29 22:13:54 -0500

    moves NavLink to own module and fixes lint issues

--- client/components/ReportsPanel/ReportsTabbar.js
+++ client/components/ReportsPanel/ReportsTabbar.js
@@ -1,29 +1,13 @@
-import React, { Component, PropTypes } from 'react';
-import { Route, Link } from 'react-router-dom';
+import React from 'react';
 
+import NavLink from 'components/NavLink';
 import styles from './style.scss';
 
-const NavLink = ({ to, label }) => (
-  <Route
-    to={to}
-    exact
-    children={({ location: { pathname } }) =>
-      <Link to={to} className={`${styles.navLink} ${pathname.indexOf(to) !== -1 ? styles.selected : ''}`}>{ label }</Link>
-    }
-  />
+const ReportsTabbar = () => (
+  <div className={styles.tabbar}>
+    <NavLink to="/reports/inflow-outflow" label="Inflow vs Outflow" styles={styles} />
+    <NavLink to="/reports/spending" label="Spending by Category" styles={styles} />
+  </div>
 );
-
-class ReportsTabbar extends Component {
-  render() {
-    return (
-      <section className={styles.reports}>
-        <div className={styles.tabbar}>
-          <NavLink to="/reports/inflow-outflow" label="Inflow vs Outflow" />
-          <NavLink to="/reports/spending" label="Spending by Category" />
-        </div>
-      </section>
-    );
-  }
-}
 
 export default ReportsTabbar;
