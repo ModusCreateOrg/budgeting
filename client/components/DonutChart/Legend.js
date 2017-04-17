462ab134f685cdfadb64eb0fd51f6af726a53a0b
Timothy Eagan <timothy.eagan@gmail.com>
2017-04-17 07:12:01 -0400

    Add amounts to Legend (#49)
    * Add amounts to Legend
    
    * PR change requests
    
    * New Line

--- client/components/DonutChart/Legend.js
+++ client/components/DonutChart/Legend.js
@@ -1,17 +1,26 @@
 import React, { PropTypes } from 'react';
 
+import LegendItem from './LegendItem';
 import styles from './styles.scss';
 
-const Legend = ({ data, color, dataLabel, dataKey }) => (
+const Legend = ({ data, color, dataValue, dataLabel, dataKey }) => (
   <ul className={styles.legend}>
     {data.map(
-      (item, idx) => <li style={{ color: color(idx) }} key={item[dataKey]}>{item[dataLabel]}</li>
+      (item, idx) => (
+        <LegendItem
+          color={color(idx)}
+          key={item[dataKey]}
+          label={item[dataLabel]}
+          value={item[dataValue]}
+        />
+      )
     )}
   </ul>
 );
 
 Legend.propTypes = {
   data: PropTypes.array.isRequired,
+  dataValue: PropTypes.string.isRequired,
   dataLabel: PropTypes.string.isRequired,
   dataKey: PropTypes.string.isRequired,
   color: PropTypes.func.isRequired,
