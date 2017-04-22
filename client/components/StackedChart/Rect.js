694d61d7f8169aebc642b0f053b83805a7bc7439
Timothy Eagan <timothy.eagan@gmail.com>
2017-04-22 15:02:23 -0400

    Animate Stacked Chart

---
+++ client/components/StackedChart/Rect.js
@@ -0,0 +1,50 @@
+import React, { Component, PropTypes } from 'react';
+
+import {
+  select,
+  interpolate
+} from 'd3';
+
+
+class Rect extends Component {
+  
+  static propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    fill: PropTypes.string.isRequired,
+    y: PropTypes.number.isRequired,
+    animDuration: PropTypes.number,
+  };
+
+  static defaultProps = {
+    animDuration: 1000
+  }
+
+  componentDidMount() {
+    const { animDuration } = this.props;
+    const rect = select(this.rectRef);
+    const interpolateY = interpolate(1000, this.rectRef.getAttribute('y'));
+
+    rect.transition()
+      .duration(animDuration * Math.random())
+      .attrTween('y', () => t => interpolateY(t))
+  }
+
+  handleRefUpdate = (ref) => {
+    this.rectRef = ref;
+  }
+
+  render() {
+    const {width, height, fill, y} = this.props;
+
+    return (
+      <rect 
+        ref={this.handleRefUpdate}
+        { ...{y, height, width, fill}}
+      />
+    );
+  }
+};
+
+
+export default Rect;
