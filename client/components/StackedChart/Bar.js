eb86dd3fba8bf58bb5d182759ec5632760d53fb8
Timothy Eagan <timothy.eagan@gmail.com>
2017-04-22 12:01:14 -0400

    Add Stacked Chart

---
+++ client/components/StackedChart/Bar.js
@@ -0,0 +1,56 @@
+import React, { Component, PropTypes } from 'react';
+
+
+class Bar extends Component {
+  
+  static propTypes = {
+    yScale: PropTypes.func.isRequired,
+    colorFn: PropTypes.func.isRequired,
+    data: PropTypes.array.isRequired,
+    transform: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+  };
+
+  componentWillMount() {
+    this.updateChartVariables();
+  }
+
+  componentWillReceiveProps({ yScale, data }) {
+    const old = this.props;
+
+    if (old.yScale !== yScale || old.data !== data) {
+      this.updateChartVariables();
+    }
+  }
+
+  updateChartVariables = () => {
+    const {yScale, data} = this.props;
+    let start = yScale.range()[0];
+
+    this.yPositions = data.map(datum => start -= yScale(datum.value));
+  }
+
+  render() {
+    const {yPositions} = this;
+    const {width, yScale, colorFn, data, transform} = this.props;
+
+    return (
+      <g transform={transform}>
+        
+        {data.map((datum, idx) => (
+          <rect 
+            key={idx}
+            y={yPositions[idx]}
+            height={yScale(datum.value)}
+            width={width}
+            fill={colorFn(idx)}
+          />
+        ))}
+
+      </g>
+    );
+  }
+};
+
+
+export default Bar;
