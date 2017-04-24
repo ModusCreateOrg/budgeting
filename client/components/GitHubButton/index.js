644238746d8e98262c4fcf8c93256d7986a6b24c
Timothy Eagan <tim@moduscreate.com>
2017-04-24 12:45:43 -0400

    GitHub button fix

--- client/components/GitHubButton/index.js
+++ client/components/GitHubButton/index.js
@@ -1,16 +1,10 @@
 import React, { PropTypes } from 'react';
 
-const getDataCountApi = type => {
-  const hash = type === 'Fork' ? 'forks' : 'stargazers';
-  return `/repos/ModusCreateOrg/budgeting-sample-app-webpack2#${hash}_count`;
+const getHref = type => {
+  const href = type === 'Fork' ? 'fork' : '';
+  return `https:/github.com/ModusCreateOrg/budgeting-sample-app-webpack2/${href}`;
 };
 
-const getDataCountHref = type => {
-  const href = type === 'Fork' ? 'network' : 'stargazers';
-  return `/ModusCreateOrg/budgeting-sample-app-webpack2/${href}`;
-};
-
-const getDataCountAriaLabel = type => `# ${type === 'Fork' ? 'forks' : 'stargazers'} on GitHub`;
 const getAriaLabel = type => `${type} ModusCreateOrg/budgeting-sample-app-webpack2 on GitHub`;
 const getDataIcon = type => (type === 'Fork' ? 'octicon-repo-forked' : 'octicon-star');
 
@@ -19,11 +13,9 @@ const GitHubButton = ({ type, className }) => (
     <a
       className="github-button"
       data-style="mega"
-      href="https://github.com/ModusCreateOrg/budgeting-sample-app-webpack2"
+      href={getHref(type)}
       data-icon={getDataIcon(type)}
-      data-count-href={getDataCountHref(type)}
-      data-show-count={getDataCountApi(type)}
-      data-count-aria-label={getDataCountAriaLabel(type)}
+      data-show-count={true}
       aria-label={getAriaLabel(type)}
     >
       {type}
