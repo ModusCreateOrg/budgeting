702e7ce44dc5a94559011d1d4986173f665ff05a
Grgur Grisogono <grgur@grgur.com>
2017-05-01 18:24:16 +0200

    github will load after the initial render

--- app/components/GitHubButton/index.js
+++ app/components/GitHubButton/index.js
@@ -1,4 +1,14 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+
+// load github buttons js
+function insertGithubApi() {
+  const tagName = 'script';
+  const target = document.getElementsByTagName(tagName)[0];
+  const tag = document.createElement(tagName);
+  tag.async = true;
+  tag.src = 'https://buttons.github.io/buttons.js';
+  target.parentNode.insertBefore(tag, target);
+}
 
 const getHref = type => {
   const href = type === 'Fork' ? 'fork' : '';
@@ -8,38 +18,35 @@ const getHref = type => {
 const getAriaLabel = type => `${type} ModusCreateOrg/budgeting-sample-app-webpack2 on GitHub`;
 const getDataIcon = type => (type === 'Fork' ? 'octicon-repo-forked' : 'octicon-star');
 
-const GitHubButton = ({ type, className }) => (
-  <div className={className}>
-    <a
-      className="github-button"
-      data-style="mega"
-      href={getHref(type)}
-      data-icon={getDataIcon(type)}
-      data-show-count
-      aria-label={getAriaLabel(type)}
-    >
-      {type}
-    </a>
-  </div>
-);
+export default class GitHubButton extends Component {
+  static propTypes = {
+    type: PropTypes.oneOf(['Fork', 'Star']).isRequired,
+    className: PropTypes.string,
+  };
 
-GitHubButton.propTypes = {
-  type: PropTypes.oneOf(['Fork', 'Star']).isRequired,
-  className: PropTypes.string,
-};
+  static defaultProps = {
+    className: '',
+  };
 
-GitHubButton.defaultProps = {
-  className: '',
-};
+  componentDidMount() {
+    insertGithubApi();
+  }
 
-// load github buttons js
-(function() {
-  const tagName = 'script';
-  const target = document.getElementsByTagName(tagName)[0];
-  const tag = document.createElement(tagName);
-  tag.async = true;
-  tag.src = 'https://buttons.github.io/buttons.js';
-  target.parentNode.insertBefore(tag, target);
-})();
-
-export default GitHubButton;
+  render() {
+    const { type, className } = this.props;
+    return (
+      <div className={className}>
+        <a
+          className="github-button"
+          data-style="mega"
+          href={getHref(type)}
+          data-icon={getDataIcon(type)}
+          data-show-count
+          aria-label={getAriaLabel(type)}
+        >
+          {type}
+        </a>
+      </div>
+    );
+  }
+}
