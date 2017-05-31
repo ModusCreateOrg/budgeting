3dba28afb63d9aeecf4ff76e5aa9f5174367c106
Grgur Grisogono <grgur@grgur.com>
2017-05-31 21:06:51 +0200

    Node Env prod

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,10 @@ module.exports = function(env) {
   ];
 
   const plugins = [
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
+    }),
+
     // create index.html
     new HtmlWebpackPlugin({
       template: './index.ejs',
