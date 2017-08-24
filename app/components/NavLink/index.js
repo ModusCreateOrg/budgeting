78e9caf2f779e06203342dba1ab550141c897eb1
Esteban Las <lasesteban@gmail.com>
2017-08-24 10:51:58 -0300

    Update to React 16.
    Currently using React 16 beta 5.
    
    Note: react-router has peer dependency warnings but it works fine.
    
    Note: "prop-types" was added as a dependency because it was removed from the core package.
    This should be removed once transition to Flow is completed.

--- app/components/NavLink/index.js
+++ app/components/NavLink/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Link } from 'react-router-dom';
 
 const NavLink = ({ to, label, styles }) =>
