3a1a05d5c6eefdc853a54effcce77d4636e1eb76
Esteban <elas7@users.noreply.github.com>
2017-08-25 02:55:21 -0300

    Update to React 16 (#80)
    * Update to React 16.
    
    Currently using React 16 beta 5.
    
    Note: react-router has peer dependency warnings but it works fine.
    
    Note: "prop-types" was added as a dependency because it was removed from the core package.
    This should be removed once transition to Flow is completed.
    
    * Update React version in Readme.md

--- app/components/DonutChart/index.js
+++ app/components/DonutChart/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Legend from 'components/Legend';
 import Chart from 'components/Chart';
 import { arc, pie, scaleOrdinal, schemeCategory20 } from 'd3';
--- app/components/GitHubButton/index.js
+++ app/components/GitHubButton/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // load github buttons js
 let gitHubApiLoaded = false;
