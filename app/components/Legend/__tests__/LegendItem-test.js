5d8cfd293de9bd5094359051a0b56bd26e573052
Esteban Las <lasesteban@gmail.com>
2017-08-26 13:33:08 -0300

    Fix console errors in jest test run.
    Add a `requestAnimationFrame` polyfill for Jest, this removes the React 16 warning "React depends on
    requestAnimationFrame" when rendering with enzyme in tests. It should be removed once Enzyme adds support
    for React 16.

--- app/components/Legend/__tests__/LegendItem-test.js
+++ app/components/Legend/__tests__/LegendItem-test.js
@@ -3,6 +3,8 @@ import renderer from 'react-test-renderer';
 import LegendItem from '../LegendItem';
 
 it('renders correctly', () => {
-  const tree = renderer.create(<LegendItem color="test" value="test" label="test" />).toJSON();
+  const tree = renderer.create(<LegendItem color="test" value={3} label="test" />).toJSON();
   expect(tree).toMatchSnapshot();
+
+  window.requestAnimationFrame(() => 'wow');
 });
