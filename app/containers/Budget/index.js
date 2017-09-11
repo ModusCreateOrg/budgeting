65e56e5fdc7b4117ba768cc69e1a31e82b5d3db8
Esteban Las <lasesteban@gmail.com>
2017-09-11 16:24:37 -0300

    Move all components connected to Redux to the `containers` folder.
    The only component moved was BudgetGrid.

--- app/containers/Budget/index.js
+++ app/containers/Budget/index.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import transactionReducer from 'modules/transactions';
 import categoryReducer from 'modules/categories';
 import { injectAsyncReducers } from 'store';
-import BudgetGrid from 'components/BudgetGrid';
+import BudgetGrid from 'containers/BudgetGrid';
 import Balance from 'containers/Balance';
 
 // inject reducers that might not have been originally there
