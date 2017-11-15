41449ffb00fe73f945bdb7b70dcb46d3d10da61b
guilherme-paschoal <guilherme@hgdata.com>
2017-11-15 19:57:12 -0200

    styling of the signs

--- app/components/BudgetItem/index.js
+++ app/components/BudgetItem/index.js
@@ -1,7 +1,10 @@
 // @flow
 import * as React from 'react';
+import formatAmount from 'utils/formatAmount';
+
 import { Route, Link } from 'react-router-dom';
 import DonutChart from 'components/DonutChart';
+import styles from './style.scss';
 
 type BugdetDetailsProps = {
   transaction: Transaction,
@@ -12,21 +15,27 @@ const BudgetItem = ({ transaction, contribution }: BugdetDetailsProps) => {
   const chartData = [
     {
       value: parseFloat(contribution.percent),
-      label: 'This'
+      label: 'This',
     },
     {
       value: 100 - parseFloat(contribution.percent),
-      label: 'Total'
-    }
+      label: 'Total',
+    },
   ];
   return (
     <section>
       <Route>
         <Link to="/budget">Back</Link>
       </Route>
-      <h1>{transaction.description}</h1>
+      <h1>
+        {transaction.description}
+        &nbsp;({formatAmount(transaction.value).text})
+      </h1>
       <h3>
-        {transaction.value}({contribution.percent}%)
+        <span className={transaction.value < 0 ? styles.redText : styles.greenText}>
+          {transaction.value > 0 ? '+' : '-'}
+        </span>
+        {formatAmount(contribution.percent, false, true).text}
       </h3>
       <DonutChart data={chartData} dataLabel="label" dataKey="label" valueAsPercent="true" />
     </section>
--- app/containers/BudgetItem/index.js
+++ app/containers/BudgetItem/index.js
@@ -1,5 +1,4 @@
 // @flow
-import * as React from 'react';
 import { injectAsyncReducers } from 'store';
 import { connect } from 'react-redux';
 import BudgetItem from 'components/BudgetItem';
