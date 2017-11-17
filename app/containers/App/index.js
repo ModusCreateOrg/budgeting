f4dba0cfa19296521889187d53f4f1eb8aeb9497
João Gabriel Lima <joaogabriellima.eng@gmail.com>
2017-11-17 01:27:28 -0300

    application settings for the Router and history that will be used for navigation

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Router, Switch, Route, Redirect} from 'react-router-dom';
 
 import ErrorBoundary from 'components/ErrorBoundary';
 import AppError from 'components/AppError';
@@ -8,19 +8,24 @@ import Header from 'components/Header';
 import Budget from 'routes/Budget';
 import Reports from 'routes/Reports';
 import Detail from 'containers/Detail';
+import createBrowserHistory from 'history/createBrowserHistory';
+
 import './style.scss';
+
+const history = createBrowserHistory();
 
 const App = () => (
   <ErrorBoundary fallbackComponent={AppError}>
     <main>
       <Header />
-
-      <Switch>
-        <Route path="/budget" component={Budget} />
-        <Route path="/reports" component={Reports} />
-        <Route path="/detail/:id" component={Detail} />
-        <Redirect to="/budget" />
-      </Switch>
+      <Router history={history}>
+        <Switch>
+          <Route path="/budget" component={Budget} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/detail/:id" component={Detail} />
+          <Redirect to="/budget" />
+        </Switch>
+      </Router>
     </main>
   </ErrorBoundary>
 );
