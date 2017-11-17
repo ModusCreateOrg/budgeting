d6d388f1ff94e638e5296c32d5a83ef83eeeeca2
João Gabriel Lima <joaogabriellima.eng@gmail.com>
2017-11-17 01:15:48 -0300

    Initializing the construction of the Detail page and testing the project

--- app/containers/Detail/index.js
+++ app/containers/Detail/index.js
@@ -1,3 +1,27 @@
 /**
  * Created by joaogabriellima on 17/11/17.
  */
+
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+import { injectAsyncReducers } from 'store';
+import transactionReducer from 'modules/transactions';
+
+import styles from './style.scss';
+
+injectAsyncReducers({
+  transactions: transactionReducer,
+});
+
+export class DetailContainer extends React.Component<DetailProps> {
+  static defaultProps = {
+    transaction: null,
+  };
+
+  render() {
+    return <h1>Detail page</h1>;
+  }
+}
+
+export default DetailContainer;
