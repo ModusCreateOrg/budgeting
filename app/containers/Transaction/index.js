a4e6e9fb01a77ac11b66527d34868bda950ae79e
Abel <abel.tamayo@gmail.com>
2018-01-12 23:45:54 +0100

    Included donut chart.

--- app/containers/Transaction/index.js
+++ app/containers/Transaction/index.js
@@ -3,9 +3,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
-import { getInflowBalance, getOutflowBalance, getTransactions } from 'selectors/transactions';
+import {
+  getInflowBalance, getOutflowBalance, getTransactions,
+  getInflowTransactions, getOutflowTransactions,
+} from 'selectors/transactions';
 import transactionReducer from 'modules/transactions';
 import { injectAsyncReducers } from 'store';
+import DonutChart from 'components/DonutChart';
 import styles from './styles.scss';
 
 injectAsyncReducers({
@@ -20,6 +24,13 @@ class Transaction extends React.Component {
     const { match: { params: { id } } } = this.props;
     const transaction = transactions.find(trans => trans.id === Number(id));
     const balance = transaction.value < 0 ? outflowBalance : inflowBalance;
+    const reducedTransactions = [
+      { id: transaction.description, value: Math.abs(transaction.value) },
+      {
+        id: `Other ${transaction.value < 0 ? 'outflow' : 'inflow'}`,
+        value: Math.abs(balance) - Math.abs(transaction.value),
+      },
+    ];
 
     return (
       <div>
@@ -33,6 +44,7 @@ class Transaction extends React.Component {
           {transaction.value < 0 && '-'}
           {`${Number(transaction.value * 100 / balance).toFixed(NUMBER_OF_DECIMALS)}%`}
         </h2>
+        <DonutChart data={reducedTransactions} dataLabel="id" dataKey="id" />
         <Link to="/budget">Back</Link>
       </div>
     );
