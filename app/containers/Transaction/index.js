071fa537e8fa0d4b103156ad57e585bd26563296
Abel <abel.tamayo@gmail.com>
2018-01-12 13:14:52 +0100

    Showing percentage of balance.

--- app/containers/Transaction/index.js
+++ app/containers/Transaction/index.js
@@ -1,6 +1,8 @@
+// @flow
 import React from 'react';
 import { connect } from 'react-redux';
-import { getTransactions } from 'selectors/transactions';
+import classNames from 'classnames';
+import { getInflowBalance, getOutflowBalance, getTransactions } from 'selectors/transactions';
 import transactionReducer from 'modules/transactions';
 import { injectAsyncReducers } from 'store';
 
@@ -8,21 +10,30 @@ injectAsyncReducers({
   transactions: transactionReducer,
 });
 
+const NUMBER_OF_DECIMALS = 2;
+
 class Transaction extends React.Component {
   render() {
-    const { transactions } = this.props;
+    const { inflowBalance, outflowBalance, transactions } = this.props;
     const { match: { params: { id } } } = this.props;
     const transaction = transactions.find(trans => trans.id === Number(id));
+    const balance = transaction.value < 0 ? outflowBalance : inflowBalance;
 
     return (
       <div>
         <h1>{transaction.description}</h1>
+        <h2 className={classNames({ positive: transaction.value >= 0, negative: transaction.value < 0 })}>
+          {transaction.value < 0 && '-'}
+          {`${Number(transaction.value * 100 / balance).toFixed(NUMBER_OF_DECIMALS)}%`}
+        </h2>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
+  inflowBalance: getInflowBalance(state),
+  outflowBalance: getOutflowBalance(state),
   transactions: getTransactions(state),
 });
 
