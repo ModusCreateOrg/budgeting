0fcd3fe187bccbdfa2d4d04be668af585f8a461c
Rex Raphael <rex.raphael@outlook.com>
2018-02-04 08:31:14 +0100

    ReactJS Challenge

--- app/containers/BudgetDetail/__test__/index-test.js
+++ app/containers/BudgetDetail/__test__/index-test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { MemoryRouter as Router, withRouter } from 'react-router'
 import { BudgetDetail } from '../index';
 
 // mock nested component
 jest.mock('containers/BudgetDetail');
 
 it('renders correctly', () => {
-  const tree = renderer.create(<BudgetDetail />).toJSON();
+  const tree = renderer.create(<Router><BudgetDetail /></Router>)).toJSON();
   expect(tree).toMatchSnapshot();
 });
