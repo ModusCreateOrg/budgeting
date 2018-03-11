c52850ae47b489bb21f50611715f731b39458e07
HiroAgustin <hiroagustin@gmail.com>
2018-03-11 19:52:41 -0300

    Add Transaction component.
    * Display Transaction description as page title.
    
    * Show percentage of transaction in balance bellow title.
    
    * Display pie chart with transaction compared to the rest of the items in the balance.
    
    * Customize chart color scheme based on transaction type (inflow/outflow).

--- app/components/Transaction/index.js
+++ app/components/Transaction/index.js
@@ -1,6 +1,44 @@
 // @flow
 import * as React from 'react';
+import { scaleLinear } from 'd3';
+import formatAmount from 'utils/formatAmount';
+import DonutChart from 'components/DonutChart';
+import styles from './style.scss';
 
-const Transaction = ({ description }) => <h1>{description}</h1>;
+const Transaction = ({ id, description, value, balance }) => {
+  const amount = Math.abs(value);
+  const percentage = formatAmount(amount / balance, false, 'percent');
+
+  const isNegative = value < 0;
+  const sign = isNegative ? '-' : '+';
+
+  const balanceLabel = isNegative ? 'Expenses' : 'Incomes';
+  const chartData = [
+    { id, description, amount },
+    { id: Infinity, description: `Other ${balanceLabel}`, amount: balance - amount },
+  ];
+
+  // Set the color scheme for the chart based on the type of transaction.
+  const scheme = [isNegative ? 'red' : 'green', 'ivory'];
+  const color = scaleLinear().range(scheme);
+
+  return (
+    <React.Fragment>
+      <h1 className={styles.title}>{description}</h1>
+      <h2 className={`${isNegative ? styles.negative : styles.positive}`}>
+        {sign}
+        {percentage.text}
+      </h2>
+      <DonutChart
+        data={chartData}
+        dataKey="id"
+        dataLabel="description"
+        dataValue="amount"
+        color={color}
+        innerRatio={Infinity}
+      />
+    </React.Fragment>
+  );
+};
 
 export default Transaction;
