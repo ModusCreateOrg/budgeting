3ac7de7197ed22f4b8386de15dd1d33bd22e9948
HiroAgustin <hiroagustin@gmail.com>
2018-03-11 18:18:47 -0300

    Extend `formatAmount` to allow for customization of style.
    * Allows for formatting amounts into percentages.
    
    * Should consider replacing parameters with object.

--- app/utils/formatAmount.js
+++ app/utils/formatAmount.js
@@ -5,10 +5,14 @@ export type FormattedAmount = {
   isNegative: boolean,
 };
 
-export default function formatAmount(amount: number, showSign: boolean = true): FormattedAmount {
+export default function formatAmount(
+  amount: number,
+  showSign: boolean = true,
+  style: 'currency' | 'percent' = 'currency'
+): FormattedAmount {
   const isNegative = amount < 0;
   const formatValue = Math.abs(amount).toLocaleString('en-us', {
-    style: 'currency',
+    style,
     currency: 'USD',
   });
 
