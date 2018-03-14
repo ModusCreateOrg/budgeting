e3f1ff411c7f5dfa0e024ea9309f2d5579afc146
jeffreyroshan <jeffreyroshan@gmail.com>
2018-03-15 00:23:37 +0530

    Added donut chart for current transaction vs other income/expense

--- app/containers/TransactionDetail/index.js
+++ app/containers/TransactionDetail/index.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom'
 import { getTransactions, getInflowBalance, getOutflowBalance } from 'selectors/transactions';
+import DonutChart from 'components/DonutChart';
 
 type TransactionDetailProps = {
   transactions: Transaction[],
@@ -32,6 +33,30 @@ class TransactionDetailContainer extends React.Component<TransactionDetailProps>
       ? (100 * Math.abs(transactionValue / outflow)).toFixed(2)
       : (100 * Math.abs(transactionValue / inflow)).toFixed(2);
 
+    const chartData = [
+      {
+        transactionId: transactionData[0].id,
+        value: Math.abs(transactionData[0].value),
+        description: transactionData[0].description,
+      },
+    ];
+
+    if (transactionData[0].value > 0) {
+      chartData.push({
+        transactionId: `${transactionData[0].id}+'other'`,
+        value: inflow - transactionData[0].value,
+        description: 'Other Income',
+      });
+    }
+
+    if (transactionData[0].value < 0) {
+      chartData.push({
+        transactionId: `${transactionData[0].id}+'other'`,
+        value: Math.abs(outflow - transactionData[0].value),
+        description: 'Other Expenses',
+      });
+    }
+
     return (
       <section>
         <h3>{transactionDesc}</h3>
@@ -44,12 +69,13 @@ class TransactionDetailContainer extends React.Component<TransactionDetailProps>
         <div>
           Value:
           {isNegative ? `-` : `+`}
+          $
           {Math.abs(transactionValue)}
         </div>
         <div>Total inflow: {inflow}</div>
         <div>Total outflow: {outflow}</div>
         <div>------</div>
-        <div>Chart -- To be placed here</div>
+        <DonutChart data={chartData} dataLabel="description" dataKey="transactionId" />
       </section>
     );
   }
