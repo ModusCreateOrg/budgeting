47fa8e179b0970a61274151d4488541dafdaa318
jeffreyroshan <jeffreyroshan@gmail.com>
2018-03-15 01:20:08 +0530

    code cleanup in TransactionDetail

--- app/containers/TransactionDetail/index.js
+++ app/containers/TransactionDetail/index.js
@@ -19,7 +19,7 @@ class TransactionDetailContainer extends React.Component<TransactionDetailProps>
     const transactionData = transactions.filter(transaction => transaction.id === transactionId);
 
     // Invalid transaction ID
-    if(transactionData.length === 0) {
+    if (transactionData.length === 0) {
       return (
         <section>
           <div>Transaction #{this.props.match.params.id} does not exist</div>
@@ -30,30 +30,38 @@ class TransactionDetailContainer extends React.Component<TransactionDetailProps>
     const transactionDesc = transactionData[0].description;
     const transactionValue = transactionData[0].value;
     const isNegative = transactionValue < 0;
+
+    // For inflow transactions, the contribution is calculated with respect to total inflow
+    // For outflow transactions, the contribution is calculated with respect to total outflow
     const contributionPercent = isNegative
       ? (100 * Math.abs(transactionValue / outflow)).toFixed(2)
       : (100 * Math.abs(transactionValue / inflow)).toFixed(2);
 
+    // Add current transation item as first item in the chart data
     const chartData = [
       {
-        transactionId: transactionData[0].id,
-        value: Math.abs(transactionData[0].value),
-        description: transactionData[0].description,
+        transactionId: `${transactionId}+''`,
+        value: Math.abs(transactionValue),
+        description: transactionDesc,
       },
     ];
 
-    if (transactionData[0].value > 0) {
+    // For inflow transaction, push the remaining inflow (ie. total inflow minus current transaction value)
+    // as second chart item for comparison
+    if (transactionValue > 0) {
       chartData.push({
-        transactionId: `${transactionData[0].id}+'other'`,
-        value: inflow - transactionData[0].value,
+        transactionId: `${transactionId}+'other'`,
+        value: inflow - transactionValue,
         description: 'Other Income',
       });
     }
 
-    if (transactionData[0].value < 0) {
+    // For outflow transaction, push the remaining outflow (ie. total outflow minus current transaction value)
+    // as second chart item for comparison
+    if (transactionValue < 0) {
       chartData.push({
-        transactionId: `${transactionData[0].id}+'other'`,
-        value: Math.abs(outflow - transactionData[0].value),
+        transactionId: `${transactionId}+'other'`,
+        value: Math.abs(outflow - transactionValue),
         description: 'Other Expenses',
       });
     }
