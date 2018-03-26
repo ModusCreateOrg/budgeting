9bf96f2b5bb1131a510b3cbf2e076e9cb9e4ba0b
slorenzo <SebastianLorenzo@gmail.com>
2018-03-26 12:58:14 -0300

    Changed Legends components to show the percentage format

--- app/components/Legend/LegendItem.js
+++ app/components/Legend/LegendItem.js
@@ -7,12 +7,13 @@ type LegendItemProps = {
   color: string,
   value: number,
   label: string,
+  percentage: Boolean,
 };
 
-const LegendItem = ({ color, label, value }: LegendItemProps) => (
+const LegendItem = ({ color, label, value, percentage }: LegendItemProps) => (
   <li style={{ color }}>
     <span>{label}</span>
-    <span className={styles.value}> {formatAmount(value).text} </span>
+    <span className={styles.value}> {formatAmount(value, true, percentage).text} </span>
   </li>
 );
 
