e40885d6a4073b91722b09b1ebfe013e9ca32e23
slorenzo <SebastianLorenzo@gmail.com>
2018-03-26 12:47:37 -0300

    Added GoBack component

---
+++ app/components/GoBack/index.js
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react';
+import styles from './styles.scss';
+
+type GoBackProps = {
+  goBack: Function,
+  text: string,
+};
+
+const GoBack = (props): GoBackProps => (
+  <div className={styles.containerGoBack} onClick={props.goBack} role="button" tabIndex="0">
+    <span>{props.text}</span>
+  </div>
+);
+
+export default GoBack;
