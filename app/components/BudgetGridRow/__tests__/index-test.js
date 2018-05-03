756b1231eb2f40e5201bb987c73b716040898379
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-04 05:21:28 +0600

    Fix linting

--- app/components/BudgetGridRow/__tests__/index-test.js
+++ app/components/BudgetGridRow/__tests__/index-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router'
+import { MemoryRouter } from 'react-router';
 import renderer from 'react-test-renderer';
 import BudgetGridRow from 'components/BudgetGridRow';
 
