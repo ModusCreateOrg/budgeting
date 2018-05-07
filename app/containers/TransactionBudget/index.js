bcd29726812148e6c9c368625ec8fcd5842989c2
Dean van Niekerk <deanvniekerk@gmail.com>
2018-05-07 23:42:24 +0200

    Added back button to transaction budget

--- app/containers/TransactionBudget/index.js
+++ app/containers/TransactionBudget/index.js
@@ -3,7 +3,8 @@ import * as React from 'react';
 import transactionReducer from 'modules/transactions';
 import { injectAsyncReducers } from 'store';
 import { withRouter } from 'react-router-dom'
-import TransactionBudgetHeader from '../../components/TransactionBudgetHeader/index'
+import TransactionBudgetHeader from 'components/TransactionBudgetHeader/index'
+import styles from './style.scss';
 
 // inject reducers that might not have been originally there
 injectAsyncReducers({
@@ -13,10 +14,11 @@ injectAsyncReducers({
 @withRouter
 export default class TransactionBudgetContainer extends React.Component {
   render() {
-    const { match: { params } } = this.props;
+    const { match: { params }, history } = this.props;
     return (
       <section>
         <TransactionBudgetHeader transactionId={params.transactionid} />
+        <button className={styles.backbutton} onClick={history.goBack}>Back</button>
       </section>
     );
   }
