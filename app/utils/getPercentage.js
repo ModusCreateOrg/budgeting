8d12a32d35c31322801afdd92501dca26da54a21
Eduardo Grajales <edgravill@gmail.com>
2018-06-27 17:24:34 -0500

    get and show percentage

---
+++ app/utils/getPercentage.js
@@ -0,0 +1,23 @@
+// @flow
+import { Transaction } from 'modules/transactions';
+
+export default function getPercentage(transaction: Transaction, transactions: Transaction[]) {
+  const values = transactions.filter(transactio => {
+    if (transaction.value < 0) {
+      return transactio.value < 0;
+    }
+
+    return transactio.value >= 0;
+  });
+
+  const percentage =
+    Math.abs(transaction.value) /
+    Math.abs(
+      values.reduce((prev, current) => ({
+        ...prev,
+        value: prev.value + current.value,
+      })).value
+    );
+
+  return `${(percentage * 100).toFixed(2)}%`;
+}
