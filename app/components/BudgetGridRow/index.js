c9ab3cda05267ad81aaf9bf7d1ea9555cf6b9a46
Eduardo Grajales <edgravill@gmail.com>
2018-06-27 20:07:41 -0500

    link

--- app/components/BudgetGridRow/index.js
+++ app/components/BudgetGridRow/index.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import formatAmount from 'utils/formatAmount';
 import getPercentage from 'utils/getPercentage';
 import type { Transaction } from 'modules/transactions';
@@ -20,24 +21,26 @@ const BudgetGridRow = ({ transaction, categories, transactions }: BudgetGridRowP
   const category = categories[categoryId];
 
   return (
-    <tr className={styles.row} key={id} title="View details">
-      <td>
-        <div className={styles.cellLabel}>Category</div>
-        <div className={styles.cellContent}>{category}</div>
-      </td>
-      <td>
-        <div className={styles.cellLabel}>Description</div>
-        <div className={styles.cellContent}>{description}</div>
-      </td>
-      <td className={amountCls}>
-        <div className={styles.cellLabel}>Amount</div>
-        <div className={styles.cellContent}>{amount.text}</div>
-      </td>
-      <td className={amountCls}>
-        <div className={styles.cellLabel}>%</div>
-        <div className={styles.cellContent}>{percentage}</div>
-      </td>
-    </tr>
+    <Link to={`/budget/${id}`} className={styles.link}>
+      <tr className={styles.row} key={id} title="View details">
+        <td>
+          <div className={styles.cellLabel}>Category</div>
+          <div className={styles.cellContent}>{category}</div>
+        </td>
+        <td>
+          <div className={styles.cellLabel}>Description</div>
+          <div className={styles.cellContent}>{description}</div>
+        </td>
+        <td className={amountCls}>
+          <div className={styles.cellLabel}>Amount</div>
+          <div className={styles.cellContent}>{amount.text}</div>
+        </td>
+        <td className={amountCls}>
+          <div className={styles.cellLabel}>%</div>
+          <div className={styles.cellContent}>{percentage}</div>
+        </td>
+      </tr>
+    </Link>
   );
 };
 
--- app/utils/getPercentage.js
+++ app/utils/getPercentage.js
@@ -1,5 +1,5 @@
 // @flow
-import { Transaction } from 'modules/transactions';
+import type { Transaction } from 'modules/transactions';
 
 export default function getPercentage(transaction: Transaction, transactions: Transaction[]) {
   const values = transactions.filter(transactio => {
