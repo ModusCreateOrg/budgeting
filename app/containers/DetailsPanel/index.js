5ee5e4e8a9b57943c9439308fa8be71d6d6c27c1
alexandarnikita <alexandar.nikita@zoho.com>
2018-12-14 21:34:18 +0100

    feat(InterviewTest): finish

--- app/containers/DetailsPanel/index.js
+++ app/containers/DetailsPanel/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { getTransactions } from 'selectors/transactions';
 import { getCategories } from 'selectors/categories';
+import find from 'lodash/find';
 import DetailsView from 'components/DetailsView';
 
 const Error404 = React.lazy(() => import('components/Error404' /* webpackChunkName: "error-msg-404" */));
@@ -17,7 +18,7 @@ export class DetailsPanel extends React.Component {
     const { id } = match.params;
 
     // Get transaction details, category
-    const transaction = (!!transactions && transactions[id]) || null;
+    const transaction = find(transactions, { id: Number.parseInt(id, 10) });
     let category = '';
 
     if (transaction && transaction.categoryId && categories && categories[transaction.categoryId]) {
