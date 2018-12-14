a3ca24457b9a405ca4528919b1dbfbd90250ab4a
alexandarnikita <alexandar.nikita@zoho.com>
2018-12-14 15:42:50 +0100

    feat(AppRoute): route update for details
    - details route with id

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -6,6 +6,7 @@ import ErrorBoundary from 'components/ErrorBoundary';
 import Header from 'components/Header';
 import Budget from 'routes/Budget';
 import Reports from 'routes/Reports';
+import Details from 'routes/Details';
 import './style.scss';
 
 const AppError = React.lazy(() => import('components/AppError' /* webpackChunkName: "error-msg" */));
@@ -18,6 +19,7 @@ const App = () => (
       <Switch>
         <Route path="/budget" component={Budget} />
         <Route path="/reports" component={Reports} />
+        <Route path="/details/:id" component={Details} />
         <Redirect to="/budget" />
       </Switch>
     </main>
