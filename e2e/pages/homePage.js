d75ce4221f1d0115015b221050e80178106c1b1c
Francisco Pedraza <pacopedraza87@gmail.com>
2019-01-24 20:54:51 -0600

    Adds two more tests in home page
    Added test to verify if description text box is clickable and displayed,
    Added test to verify if value text box is clickable and displayed
    
    Signed-off-by: Francisco Pedraza <pacopedraza87@gmail.com>

--- e2e/pages/homePage.js
+++ e2e/pages/homePage.js
@@ -1,23 +1,46 @@
 const Page = require('../base/page.js');
 
 const homePage = Object.create(Page, {
-  //= ===========================================================================
-  //= ======================== APP ELEMENTS =====================================
-  //= ===========================================================================
+  //============================================================================
+  // ======================== APP ELEMENTS =====================================
+  //============================================================================
   addButton: {
     get: function() {
       return browser.element("//button[contains(text(), 'Add')]");
     },
   },
+  descriptionTextBox: {
+    get: function() {
+      return browser.element("input[name = description");
+    },
+  },
+  valueTextBox: {
+    get: function() {
+      return browser.element("input[name = value");
+    },
+  },
 
-  //= ===========================================================================
-  //= ======================== TEST ACTIONS =====================================
-  //= ===========================================================================
+  //============================================================================
+  //========================= TEST ACTIONS =====================================
+  //============================================================================
   verifyAddButton: {
     value: function() {
       return this.addButton.isEnabled();
     },
   },
+  enterDescription: {
+    value: function() {
+      this.descriptionTextBox.waitForVisible();
+      this.descriptionTextBox.click();
+    },
+  },
+  enterValue: {
+    value: function() {
+      this.valueTextBox.waitForVisible();
+      this.valueTextBox.click();
+    },
+  },
+
 });
 
 module.exports = homePage;
--- e2e/tests/addCategory.js
+++ e2e/tests/addCategory.js
@@ -3,9 +3,17 @@ const homePage = require('../pages/homePage.js');
 
 describe('Welcome Page', () => {
   it('Verifies Add button exists and is disabled', () => {
-    //XXX: Below url call is going to be fixed, I had issues with chromedriver. 
+    // XXX: Below url call is going to be fixed, I had issues with chromedriver.
     // For now working in this way.
     browser.url('/');
     expect(homePage.verifyAddButton()).to.be.false;
+  });
+
+  it('Verifies that description text box is displayed and clickable', function() {
+    homePage.enterDescription();
+  });
+
+  it('Verifies that value text box is displayed and clickable', function() {
+    homePage.enterValue();
   });
 });
