a00b2e2d35e3bd37a826825858dfd9fefb955ddd
Francisco Pedraza <pacopedraza87@gmail.com>
2019-01-24 20:28:09 -0600

    Removes unuseful lines on package.json

--- e2e/tests/addCategory.js
+++ e2e/tests/addCategory.js
@@ -3,6 +3,8 @@ const homePage = require('../pages/homePage.js');
 
 describe('Welcome Page', () => {
   it('Verifies Add button exists and is disabled', () => {
+    //XXX: Below url call is going to be fixed, I had issues with chromedriver. 
+    // For now working in this way.
     browser.url('/');
     expect(homePage.verifyAddButton()).to.be.false;
   });
